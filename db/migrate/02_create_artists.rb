96d8a19a0a1f4a6e623935996788e7f6cfe62fee
fidelscodes <fidelscodes@gmail.com>
2016-08-02 10:50:02 -0400

    Update with solution

--- db/migrate/02_create_artists.rb
+++ db/migrate/02_create_artists.rb
@@ -1,2 +1,10 @@
+# An artist has_many songs
+# Also has_many genres, through songs
+
 class CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+    end
+  end
 end
