58835a82cfaa330b30fcd572184888a17efbc760
ozPop <ozziepopovas@gmail.com>
2016-08-03 15:18:41 -0400

    populate models and migrations

--- db/migrate/01_create_songs.rb
+++ db/migrate/01_create_songs.rb
@@ -1,2 +1,11 @@
 class CreateSongs < ActiveRecord::Migration
+
+  def change
+    create_table :songs do |col|
+      col.string :name
+      col.integer :artist_id
+      col.integer :genre_id
+    end
+  end
+
 end
